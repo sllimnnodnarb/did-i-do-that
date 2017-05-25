09e807245df4f191a5fb88cdcccdc9318c77417c
Brandon Mills <sllimnodnarb@gmail.com>
2017-05-24 22:59:05 -0500

    adding all files associated with user-story-8-rake-delete

--- did-i-do-that/db/seeds.rb
+++ did-i-do-that/db/seeds.rb
@@ -1,21 +1,22 @@
 5.times do
-  User.create!(
+  user = User.create!(
     email: Faker::Internet.email,
     password: Faker::Internet.password(8)
   )
-  confirmed_at: Time.now
+  user.update_attributes!(confirmed_at: Time.now)
 end
 users = User.all
 
-5.times do
-  Item.create!(
+20.times do
+  item = Item.create!(
     user: users.sample,
     name: Faker::Name.name,
   )
+  item.update_attributes!(created_at: 10.days.ago)
 end
 items = Item.all
 
 puts "#{User.count} users created"
-puts "#{Item.count} wikis created"
+puts "#{Item.count} items created"
 puts "Seed finished"
 puts "Some seeds fell beside the road, and the birds came and ate them up..."
